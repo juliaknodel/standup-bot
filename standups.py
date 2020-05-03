b59ce5edf66101a57ead70d12540a661bf1dc561
juliaknodel <juliaknodel13@gmail.com>
2020-05-04 01:57:32 +0300

    Add ability to enter days in any case

--- standups.py
+++ standups.py
@@ -36,7 +36,6 @@ def show_standups(update, context):
     context.bot.send_message(chat_id=user_chat_id, text=message_text)
 
 
-
 def set_standups(update, context):
     # TODO: добавить проверку, является ли пользователь администратором в команде
     chat_id = update.effective_chat.id
@@ -172,12 +171,12 @@ def check_standups_input(chat_id, args):
         time_ind = arg_ind + 1
         period_ind = arg_ind + 2
 
-        if args[day_ind] not in ALL_DAYS:
+        if args[day_ind].upper() not in ALL_DAYS:
             return args[day_ind] + " - недопустимое значение дня недели."
-        if args[day_ind] in standup_days:
+        if args[day_ind].upper() in standup_days:
             return "День " + args[day_ind] + " введен дважды."
         else:
-            standup_days.append(args[day_ind])
+            standup_days.append(args[day_ind].upper())
 
         if is_time_value(args[time_ind]) is False:
             return args[time_ind] + " - недопустимое значение времени."
@@ -235,7 +234,7 @@ def write_schedule_to_db(args, team_db_id):
         time_ind = arg_ind + 1
         period_ind = arg_ind + 2
         hours, mins = get_time(args[time_ind])
-        schedule.append({"day": args[day_ind], "hours": hours, "minutes": mins, "period": args[period_ind]})
+        schedule.append({"day": args[day_ind].upper(), "hours": hours, "minutes": mins, "period": args[period_ind]})
     collection.teams.update_one({"_id": team_db_id}, {"$set": {"schedule": schedule}})
 
 
